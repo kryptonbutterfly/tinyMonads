c6e7160768a49749912029f75c4039449491aca5
tinycodecrank <tinycodecrank@gmail.com>
2022-08-02 09:39:22 +0200

    add missing export to module-info

--- src/module-info.java
+++ src/module-info.java
@@ -3,6 +3,8 @@ module de.tinycodecrank.Monads
 	exports de.tinycodecrank.monads;
 	exports de.tinycodecrank.monads.opt;
 	exports de.tinycodecrank.monads.either;
+	exports de.tinycodecrank.monads.oneof3;
+	exports de.tinycodecrank.monads.failable;
 	
 	requires de.tinycodecrank.Functional;
 	requires de.tinycodecrank.Iterator;
